3fc75cbc74ab6a9da29c4b287f93c7e7c2910500
Julio Marquez <juliocmrqzm@gmail.com>
2020-07-14 14:00:59 -0300

    perf(website): lazy load the intersection-observer polyfill

--- plugins/vanilla-lazyload.client.ts
+++ plugins/vanilla-lazyload.client.ts
@@ -1,45 +1,56 @@
 import Vue, { PluginObject } from 'vue'
 import { Plugin } from '@nuxt/types'
 
-require('intersection-observer')
+const LazyLoadPlugin: Plugin = async () => {
+  if (
+    !(
+      'IntersectionObserver' in window &&
+      'IntersectionObserverEntry' in window &&
+      'intersectionRatio' in window.IntersectionObserverEntry.prototype
+    )
+  ) {
+    await import(
+      // @ts-ignore
+      /* webpackChunkName: 'intersection-observer' */ 'intersection-observer'
+    )
+  }
 
-const LazyLoadPlugin: Plugin = () => {
-  return import(
+  const { default: LazyLoad } = await import(
     /* webpackChunkName: 'vanilla-lazyload' */ 'vanilla-lazyload'
-  ).then(({ default: LazyLoad }) => {
-    const lazyLoadPlugin: PluginObject<any> = {
-      install: (Vue) => {
-        let lazyLoadInstance: typeof LazyLoad | null = null
+  )
 
-        function updateLazy() {
-          if (lazyLoadInstance) {
-            lazyLoadInstance.update()
-          } else {
-            lazyLoadInstance = new LazyLoad({
-              elements_selector: '.responsive-image .lazy',
-            })
-          }
+  const lazyLoadPlugin: PluginObject<any> = {
+    install: (Vue) => {
+      let lazyLoadInstance: typeof LazyLoad | null = null
+
+      function updateLazy() {
+        if (lazyLoadInstance) {
+          lazyLoadInstance.update()
+        } else {
+          lazyLoadInstance = new LazyLoad({
+            elements_selector: '.responsive-image .lazy',
+          })
         }
+      }
 
-        Vue.mixin({
-          updated() {
-            // @ts-ignore
-            this.$nextTick(function () {
-              updateLazy()
-            })
-          },
-          mounted() {
-            // @ts-ignore
-            this.$nextTick(function () {
-              updateLazy()
-            })
-          },
-        })
-      },
-    }
+      Vue.mixin({
+        updated() {
+          // @ts-ignore
+          this.$nextTick(function () {
+            updateLazy()
+          })
+        },
+        mounted() {
+          // @ts-ignore
+          this.$nextTick(function () {
+            updateLazy()
+          })
+        },
+      })
+    },
+  }
 
-    Vue.use(lazyLoadPlugin)
-  })
+  Vue.use(lazyLoadPlugin)
 }
 
 export default LazyLoadPlugin
